b369983745ba71a63203c7b6bf4b3e165b7ba005
VidhyaRatheesh <vidhyanair2018@gmail.com>
2022-02-11 08:26:15 +0530

    commit

--- src/handlingdropdowns/Booking.java
+++ src/handlingdropdowns/Booking.java
@@ -16,45 +16,54 @@ public class Booking {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\LENOVO\\Desktop\\chrome\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.get("https://www.booking.com/");
 		driver.findElement(By.xpath("//span[normalize-space()='Flights']")).click();
 		driver.findElement(By.xpath("//div[contains(text(),'Round-trip')]")).click();
 		driver.findElement(By.cssSelector(".css-1k0jlfl")).click();
-		List<WebElement> options = driver.findElements(By.className("css-1k0jlfl"));		
-		for (WebElement option : options) {
-			if (option.getText().equalsIgnoreCase("4")) {
-				option.click();
-			}
-		}
+		WebElement staticdropdown = driver.findElement(By.cssSelector(".css-1k0jlfl"));
+		Select dropdown = new Select(staticdropdown);
+		dropdown.selectByIndex(1);
+		System.out.println(dropdown.getFirstSelectedOption().getText());
+
+		driver.findElement(By.xpath("(//div[contains(text(),'1 adult')])[1]")).click();
+		driver.findElement(By.cssSelector(".css-153jucu")).click();
+		driver.findElement(By.xpath("(//button[@class='css-153jucu'])[2]")).click();
 		
-		driver.findElement(By.cssSelector("div[class='css-vvcyi3'] div div div")).click();
-		driver.findElement(By.cssSelector(".css-153jucu")).click();	
-		WebElement staticdropdown1 = driver.findElement(By.className("css-153jucu"));
-		Select dropdown1 = new Select(staticdropdown1);
-		dropdown1.selectByIndex(3);
-		System.out.println(dropdown1.getFirstSelectedOption().getText());
-		
-		//driver.findElement(By.cssSelector("Text-module__root--variant-strong_2___1U9wM")).click();
-		//driver.findElement(By.cssSelector(".css-153jucu")).click();
-		
-		
-		
+		Select ChildAge = new Select(driver.findElement(By.xpath("(//select[@id='__bui-6'])")));
+		ChildAge.selectByValue("6");
+		System.out.println(ChildAge.getFirstSelectedOption().getText());
+		 
+
+		driver.findElement(By.cssSelector(".css-ya5gr9")).click();
+
 		driver.findElement(By.cssSelector(".css-p6nr6s")).click();
 		driver.findElement(By.cssSelector(".css-1eii3rq")).click();
 		driver.findElement(By.xpath("//input[@placeholder='Where from?']")).sendKeys("Trivandrum");
+		List<WebElement> selections = driver.findElements(By.xpath("//div[@class='css-1bbz8d7 14px']"));
+		for (WebElement selection : selections) {
+			if (selection.getText().equalsIgnoreCase("TRV")) {
+				selection.click();
+			}
+		}
+
 		driver.findElement(By.cssSelector("input[class='css-bwf0ll']")).click();
 		driver.findElement(By.cssSelector("input[class='css-bwf0ll']")).sendKeys(Keys.ESCAPE);
 		driver.findElement(By.className("css-g0pg3f-SearchboxInput")).click();
 		driver.findElement(By.cssSelector("input[class='css-1tl2oa1']")).sendKeys("Delhi");
+		List<WebElement> selections1 = driver.findElements(By.xpath("//div[@class='css-1bbz8d7 14px']"));
+		for (WebElement selection : selections1) {
+			if (selection.getText().equalsIgnoreCase("DEL")) {
+				selection.click();
+			}
+		}
 		driver.findElement(By.cssSelector("input[class='css-bwf0ll']")).click();
 		driver.findElement(By.cssSelector("input[class='css-bwf0ll']")).sendKeys(Keys.ESCAPE);
 		driver.findElement(By.cssSelector("input[placeholder='Depart']")).click();
-		driver.findElement(By.cssSelector("span[data-date-cell='2022-02-10']")).click();
+		driver.findElement(By.cssSelector("span[data-date-cell='2022-02-12']")).click();
 		driver.findElement(By.cssSelector("input[placeholder='Return']")).click();
 		driver.findElement(By.cssSelector("span[data-date-cell='2022-02-16']")).click();
 		driver.findElement(By.xpath("//button[normalize-space()='Search']")).click();
-        driver.close();
-     }
+		driver.close();
+	}
 }
-
